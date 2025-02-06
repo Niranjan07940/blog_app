0b611f14a66f217a1263235f220e8cd5d78b4eac
Niranjan <niranjanreddyp769692@gmail.com>
2025-02-06 18:11:54 +0530

    fixes to jwtWebsecurity

--- demo/src/main/java/com/niran/demo/Config/WebSecurityConfig.java
+++ demo/src/main/java/com/niran/demo/Config/WebSecurityConfig.java
@@ -32,8 +32,8 @@ public class WebSecurityConfig {
         httpSecurity
                 .csrf(csrf-> csrf.disable())
                 .authorizeHttpRequests(
-                request-> request               //"register","sign","blogpost","getImg","sendOtp","verifyOtp","updatePwd","getBlogs","/**","getByUname","getProfile","deleteBlog"
-                        .requestMatchers("register","sign","sendOtp","verifyOtp","updatePwd","/**").permitAll()
+                request-> request               //
+                        .requestMatchers("register","sign","blogpost","getImg","sendOtp","verifyOtp","updatePwd","getBlogs","/**","getByUname","getProfile","deleteBlog").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
